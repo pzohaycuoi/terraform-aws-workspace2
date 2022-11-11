f92ee51b928e299ea1f5cc638eec965f8a39c074
pzohaycuoi <pzohaycuoi@gmail.com>
2022-11-11 18:42:45 +0700

    initial commit

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  cloud {
+    organization = "nambeotest"
+    hostname = "app.terraform.io"
+
+    workspaces {
+      name = "terraform-aws-workspace2"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+data "terraform_remote_state" "network" {
+  backend = "remote"
+
+  config = {
+    organization = "nambeotest"
+    workspaces = {
+          name = "terraform-aws-workspace1"
+    }
+  }
+}
+
+module "namebeovpc" {
+  source = "app.terraform.io/nambeotest/nambeoec2/aws"
+  subnet = data.terraform_remote_state.network.private_subnet
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "region" {
+  description = "region to provision resources"
+  type = string
+  default = "ap-southeast-1"
+}
