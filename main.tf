64e2cd6eb49c2349c8cf8ab73a9c9508a4e517ce
pzohaycuoi <pzohaycuoi@gmail.com>
2022-11-12 19:05:35 +0700

    hmmm

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-terraform {
-  cloud {
-    organization = "nambeotest"
-    hostname = "app.terraform.io"
-
-    workspaces {
-      name = "terraform-aws-workspace2"
-    }
-  }
-}
-
 provider "aws" {
   region = var.region
 }
@@ -19,14 +8,14 @@ data "terraform_remote_state" "vpc" {
   config = {
     organization = "nambeotest"
     workspaces = {
-          name = "terraform-aws-workspace1"
+      name = "terraform-aws-workspace1"
     }
   }
 }
 
-module "namebeovpc" {
-  source = "app.terraform.io/nambeotest/nambeoec2/aws"
-  region = data.terraform_remote_state.vpc.outputs.region
-  subnet = data.terraform_remote_state.vpc.outputs.public_subnet
+module "namebeoec2" {
+  source       = "app.terraform.io/nambeotest/nambeoec2/aws"
+  region       = data.terraform_remote_state.vpc.outputs.region
+  subnet       = data.terraform_remote_state.vpc.outputs.public_subnet
   project_name = data.terraform_remote_state.vpc.outputs.project_name
 }
